a6e924d04a1edb163d377bd6d1f0b932659a2c0e
Brayanibarguen <142102076+Brayanibarguen@users.noreply.github.com>
2023-11-17 12:01:34 -0500

    Delete ejemplo/estudiantes directory

--- ejemplo/estudiantes/infrastructure/controller/VerEstudianteService.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplo.estudiantes.infrastructure.controller;
-
-import com.ejemplo.estudiantes.domain.Estudiante;
-
-import java.util.List;
-
-public interface VerEstudianteService {
-    List<Estudiante> obtenerEstudiantes();
-}
